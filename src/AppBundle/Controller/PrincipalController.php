e0038c854be9410c4ec8816f62fc00cca937f316
Antonio Carrillo <antoniocarrillo1990@gmail.com>
2018-10-01 19:18:21 +0200

    gestionando registro con roles

--- src/AppBundle/Controller/PrincipalController.php
+++ src/AppBundle/Controller/PrincipalController.php
@@ -15,6 +15,41 @@ class PrincipalController extends Controller
     }
 
     public function registroAction(Request $request){
+        
+        $data = $request->request->all();
+
+        $em = $this->getDoctrine()->getManager();
+
+        $existeAlumno = $em->getRepository('AppBundle:Alumno')->findOneBy(['email' => $data['email']]);
+        $existeProfesor = $em->getRepository('AppBundle:Profesor')->findOneBy(['email' => $data['email']]);
+        $existeEmpresa = $em->getRepository('AppBundle:Empresa')->findOneBy(['email' => $data['email']]);
+
+        if($existeAlumno==null && $existeEmpresa==null && $existeProfesor==null){
+
+            if($data['role']=='ROL_ALUMNO'){
+                return $this->render('principal/alumnoIndex.html.twig', array(
+                    'email' => $data['email'],
+                    'password' => $data['password'],
+                ));
+                }elseif($data['role']=='ROL_PROFESOR'){
+                    return $this->render('principal/profesorIndex.html.twig', array(
+                        'email' => $data['email'],
+                        'password' => $data['password'],
+                    ));
+                }else{
+                    return $this->render('principal/empresaIndex.html.twig', array(
+                        'email' => $data['email'],
+                        'password' => $data['password'],
+                    ));
+                }
+        }else{
+            $error="Usuario ya registrado";
+            return $this->render('principal/index.html.twig', array(
+                'error' => $error
+            ));
+        }
+
+
 
     }
 }
